3a987cc9f931eab04856248f22cf54d1b821b577
saif64s <my@wo.rk>
2024-10-07 10:28:41 +0600

    app colors added

--- lib/core/configs/app_colors.dart
+++ lib/core/configs/app_colors.dart
@@ -1,7 +1,85 @@
 import 'dart:ui';
 
 class AppColors {
-  static const primary = Color(0xff42C83C);
-  static const lightBackground = Color(0xffF2F2F2);
-  static const darkBackground = Color(0xff0D0C0C);
+  // Text colors
+  static const textActionPrimaryLight = Color(0xFFFFFFFF);
+  static const textActionPrimaryDark = Color(0xFF15212F);
+
+  static const textActionSecondaryLight = Color(0xFF2970FF);
+  static const textActionSecondaryDark = Color(0xFFB2CFFF);
+
+  static const textActionTertiaryLight = Color(0xFF528BFF);
+  static const textActionTertiaryDark = Color(0xFFD1E0FF);
+
+  static const textActionDisableLight = Color(0xFFD1D5DB);
+  static const textActionDisableDark = Color(0xFFD1D5DB);
+
+  static const textTertiaryLight = Color(0xFF9CA3AF);
+  static const textTertiaryDark = Color(0xFF6B7280);
+
+  static const textSecondaryLight = Color(0xFF454955);
+  static const textSecondaryDark = Color(0xFF9CA3AF);
+
+  static const textPrimaryLight = Color(0xFF15212F);
+  static const textPrimaryDark = Color(0xFFFFFFFF);
+
+  static const textDisableLight = Color(0xFFD1D5DB);
+  static const textDisableDark = Color(0xFFD1D5DB);
+
+  // Background colors
+  static const backgroundPrimaryLight = Color(0xFFF6F7FB);
+  static const backgroundPrimaryDark = Color(0xFF15212F);
+
+  static const backgroundSecondaryLight = Color(0xFFD1D5DB);
+  static const backgroundSecondaryDark = Color(0xFF030712);
+
+  static const backgroundTertiaryLight = Color(0xFFFFFFFF);
+  static const backgroundTertiaryDark = Color(0xFF111827);
+
+  static const backgroundActionPrimaryLight = Color(0xFF2970FF);
+  static const backgroundActionPrimaryDark = Color(0xFF528BFF);
+
+  static const backgroundActionSecondaryLight = Color(0xFF155EEF);
+  static const backgroundActionSecondaryDark = Color(0xFF155EEF);
+
+  static const backgroundActionTertiaryLight = Color(0xFF84ADFF);
+  static const backgroundActionTertiaryDark = Color(0xFF84ADFF);
+
+  static const backgroundDisableMainLight = Color(0xFF6B7280);
+  static const backgroundDisableMainDark = Color(0xFF6B7280);
+
+  static const backgroundDisableOptionLight = Color(0xFF9CA3AF);
+  static const backgroundDisableOptionDark = Color(0xFF9CA3AF);
+
+  static const shadePrimaryLight = Color(0xFFD1E0FF);
+  static const shadePrimaryDark = Color(0xFFD1E0FF);
+
+  static const shadeSecondaryLight = Color(0xFFEFF4FF);
+  static const shadeSecondaryDark = Color(0xFFEFF4FF);
+
+  static const shadeNeutralLight = Color(0xFFE5E7EB);
+  static const shadeNeutralDark = Color(0xFFE5E7EB);
+
+  // Border colors
+  static const borderFocusPrimaryLight = Color(0xFF2970FF);
+  static const borderFocusPrimaryDark = Color(0xFF84ADFF);
+
+  static const borderFocusSecondaryLight = Color(0xFF84ADFF);
+  static const borderFocusSecondaryDark = Color(0xFFB2CFFF);
+
+  static const borderNormalLight = Color(0xFFE5E7EB);
+  static const borderNormalDark = Color(0xFF528BFF);
+
+  // Icon colors
+  static const iconBrandLight = Color(0xFF2970FF);
+  static const iconBrandDark = Color(0xFF2970FF);
+
+  static const iconPrimaryLight = Color(0xFF15212F);
+  static const iconPrimaryDark = Color(0xFFFFFFFF);
+
+  static const iconSecondaryLight = Color(0xFFFFFFFF);
+  static const iconSecondaryDark = Color(0xFF15212F);
+
+  static const iconDisableLight = Color(0xFFD1D5DB);
+  static const iconDisableDark = Color(0xFFD1D5DB);
 }
