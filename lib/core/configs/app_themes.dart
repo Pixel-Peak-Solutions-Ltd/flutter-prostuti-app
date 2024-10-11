e785a4552cd83e2d4345296d70af04160d6bdd00
saif64s <my@wo.rk>
2024-10-11 19:07:49 +0600

    AppTheme

--- lib/core/configs/app_themes.dart
+++ lib/core/configs/app_themes.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+
 import 'app_colors.dart';
+
+/*
+24px: titleLarge
+20px: titleMedium
+18px: titleSmall
+16px: bodyLarge
+14px: bodyMedium
+12px: bodySmall
+ */
 
 class AppTheme {
   static final lightTheme = ThemeData(
@@ -8,30 +18,53 @@ class AppTheme {
     scaffoldBackgroundColor: AppColors.backgroundPrimaryLight,
     brightness: Brightness.light,
     textTheme: GoogleFonts.hindSiliguriTextTheme().copyWith(
-      bodyLarge: const TextStyle(
-          color: AppColors.textPrimaryLight), // Set for body text
       titleLarge: const TextStyle(
-          color: AppColors.textSecondaryLight), // Set for headlines
+        color: AppColors.textPrimaryLight,
+        fontSize: 24,
+        fontWeight: FontWeight.w700,
+      ),
+      titleMedium: const TextStyle(
+        color: AppColors.textPrimaryLight,
+        fontSize: 20,
+        fontWeight: FontWeight.w700,
+      ),
+      titleSmall: const TextStyle(
+        color: AppColors.textPrimaryLight,
+        fontSize: 18,
+        fontWeight: FontWeight.w600,
+      ),
+      bodyLarge: const TextStyle(
+        color: AppColors.textPrimaryLight,
+        fontSize: 16,
+        fontWeight: FontWeight.w500,
+      ),
+      bodyMedium: const TextStyle(
+        color: AppColors.textPrimaryLight,
+        fontSize: 14,
+        fontWeight: FontWeight.w400,
+      ),
       bodySmall: const TextStyle(
-          color: AppColors.textTertiaryLight), // Set for captions
+        color: AppColors.textPrimaryLight,
+        fontSize: 12,
+        fontWeight: FontWeight.w400,
+      ),
     ),
     inputDecorationTheme: InputDecorationTheme(
-      filled: true,
       fillColor: AppColors.backgroundSecondaryLight,
-      contentPadding: const EdgeInsets.all(30),
+      // contentPadding: const EdgeInsets.all(30),
       hintStyle: const TextStyle(
         color: AppColors.textTertiaryLight,
         fontWeight: FontWeight.w500,
       ),
       border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(30),
+        borderRadius: BorderRadius.circular(8),
         borderSide:
-            const BorderSide(color: AppColors.borderNormalLight, width: 0.4),
+            const BorderSide(color: AppColors.borderNormalLight, width: 1),
       ),
       enabledBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(30),
+        borderRadius: BorderRadius.circular(8),
         borderSide: const BorderSide(
-            color: AppColors.borderFocusPrimaryLight, width: 0.4),
+            color: AppColors.borderFocusPrimaryLight, width: 1),
       ),
     ),
   );
@@ -41,30 +74,52 @@ class AppTheme {
     scaffoldBackgroundColor: AppColors.backgroundPrimaryDark,
     brightness: Brightness.dark,
     textTheme: GoogleFonts.hindSiliguriTextTheme().copyWith(
-      bodyLarge: const TextStyle(
-          color: AppColors.textPrimaryDark), // Dark mode body text
       titleLarge: const TextStyle(
-          color: AppColors.textSecondaryDark), // Dark mode headlines
+        color: AppColors.textPrimaryDark,
+        fontSize: 24,
+        fontWeight: FontWeight.w700,
+      ),
+      titleMedium: const TextStyle(
+        color: AppColors.textPrimaryDark,
+        fontSize: 20,
+        fontWeight: FontWeight.w700,
+      ),
+      titleSmall: const TextStyle(
+        color: AppColors.textPrimaryDark,
+        fontSize: 18,
+        fontWeight: FontWeight.w600,
+      ),
+      bodyLarge: const TextStyle(
+        color: AppColors.textPrimaryDark,
+        fontSize: 16,
+        fontWeight: FontWeight.w500,
+      ),
+      bodyMedium: const TextStyle(
+        color: AppColors.textPrimaryDark,
+        fontSize: 14,
+        fontWeight: FontWeight.w400,
+      ),
       bodySmall: const TextStyle(
-          color: AppColors.textTertiaryDark), // Dark mode captions
+        color: AppColors.textPrimaryDark,
+        fontSize: 12,
+        fontWeight: FontWeight.w400,
+      ),
     ),
     inputDecorationTheme: InputDecorationTheme(
-      filled: true,
       fillColor: AppColors.backgroundSecondaryDark,
-      contentPadding: const EdgeInsets.all(30),
       hintStyle: const TextStyle(
         color: AppColors.textTertiaryDark,
         fontWeight: FontWeight.w500,
       ),
       border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(30),
+        borderRadius: BorderRadius.circular(8),
         borderSide:
-            const BorderSide(color: AppColors.borderNormalDark, width: 0.4),
+            const BorderSide(color: AppColors.borderNormalDark, width: 1),
       ),
       enabledBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(30),
-        borderSide: const BorderSide(
-            color: AppColors.borderFocusPrimaryDark, width: 0.4),
+        borderRadius: BorderRadius.circular(8),
+        borderSide:
+            const BorderSide(color: AppColors.borderFocusPrimaryDark, width: 1),
       ),
     ),
   );
--- lib/features/onboarding/view/onboarding_page.dart
+++ lib/features/onboarding/view/onboarding_page.dart
@@ -1,35 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:prostuti/common/helpers/theme_provider.dart';
 
 class OnboardingPage extends ConsumerWidget {
   const OnboardingPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, ref) {
+    const decoration = InputDecoration();
+
+    final decorationWithThemeProperties =
+        decoration.applyDefaults(const InputDecorationTheme(
+      filled: true,
+      fillColor: Colors.red,
+    ));
     return Scaffold(
-      body: Center(
-        child: Row(
-          children: [
-            ElevatedButton(
-              onPressed: () =>
-                  ref.read(themeNotifierProvider.notifier).setLightTheme(),
-              child: Text('light'),
-            ),
-            Text(
-              'Hello, themed world!',
-              style: Theme.of(context)
-                  .textTheme
-                  .bodyLarge, // Uses the theme's text style
-            ),
-            ElevatedButton(
-              onPressed: () =>
-                  ref.read(themeNotifierProvider.notifier).setDarkTheme(),
-              child: Text('dark'),
-            ),
-          ],
-        ),
-      ),
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      body: Scaffold(),
     );
   }
 }
