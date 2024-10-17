99bd957378c8be90cd9a99211cfed20f242eba08
saif64s <my@wo.rk>
2024-10-17 20:41:19 +0600

    fixed mason bricks 2

---
+++ lib/features/onboarding/repository/onboarding_repo.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'onboarding_repo.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$onboardingRepoHash() => r'6bc6d5bb9176597cfb35dc091bebb651f898b6ad';
+
+/// See also [onboardingRepo].
+@ProviderFor(onboardingRepo)
+final onboardingRepoProvider = AutoDisposeProvider<OnboardingRepo>.internal(
+  onboardingRepo,
+  name: r'onboardingRepoProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$onboardingRepoHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef OnboardingRepoRef = AutoDisposeProviderRef<OnboardingRepo>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
