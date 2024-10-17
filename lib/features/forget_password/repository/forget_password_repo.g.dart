82f688da63414461a8e0dcebcf03107e62f09182
saif64s <my@wo.rk>
2024-10-17 23:29:57 +0600

    forget password screen done

---
+++ lib/features/forget_password/repository/forget_password_repo.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'forget_password_repo.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$forgetPasswordRepoHash() =>
+    r'12284bee210d6a47a8bf470555e860c3ebb00ab3';
+
+/// See also [forgetPasswordRepo].
+@ProviderFor(forgetPasswordRepo)
+final forgetPasswordRepoProvider =
+    AutoDisposeProvider<ForgetPasswordRepo>.internal(
+  forgetPasswordRepo,
+  name: r'forgetPasswordRepoProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$forgetPasswordRepoHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef ForgetPasswordRepoRef = AutoDisposeProviderRef<ForgetPasswordRepo>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
---
+++ lib/features/forget_password/view/forget_password_view.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gap/gap.dart';
+
+import '../../../common/widgets/long_button.dart';
+import '../../signup/view/otp_view.dart';
+import '../../signup/viewmodel/phone_number_viewmodel.dart';
+
+class ForgetPasswordView extends ConsumerStatefulWidget {
+  const ForgetPasswordView({Key? key}) : super(key: key);
+
+  @override
+  ForgetPasswordViewState createState() => ForgetPasswordViewState();
+}
+
+class ForgetPasswordViewState extends ConsumerState<ForgetPasswordView> {
+  final _phoneController = TextEditingController();
+
+  @override
+  void dispose() {
+    _phoneController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        automaticallyImplyLeading: false,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(
+                "assets/images/prostuti_logo.png",
+                width: 154,
+                height: 101,
+              ),
+              const Gap(118),
+              Text(
+                'পাসওয়ার্ড ভুলে গেছেন',
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+              const Gap(32),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    'ফোন নম্বর',
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  ),
+                  const Gap(6),
+                  TextFormField(
+                    keyboardType: TextInputType.number,
+                    controller: _phoneController,
+                    decoration: const InputDecoration(
+                        hintText: "আপনার ফোন নম্বর লিখুন"),
+                  ),
+                ],
+              ),
+              const Gap(32),
+              LongButton(
+                text: 'এগিয়ে যাই',
+                onPressed: () {
+                  ref
+                      .watch(phoneNumberProvider.notifier)
+                      .setPhoneNumber(_phoneController.text);
+                  Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => const OtpView(),
+                  ));
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
