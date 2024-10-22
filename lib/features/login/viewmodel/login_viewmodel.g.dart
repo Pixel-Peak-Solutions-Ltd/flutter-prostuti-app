1b0fc75c8a2ee16835163d61be20393d26db1015
saif64s <my@wo.rk>
2024-10-22 15:30:07 +0600

    auth done without refreshToken

---
+++ lib/features/login/viewmodel/login_viewmodel.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'login_viewmodel.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$rememberMeHash() => r'1a21d53143828ed4915f20ec220ec928f4930595';
+
+/// See also [RememberMe].
+@ProviderFor(RememberMe)
+final rememberMeProvider =
+    AutoDisposeNotifierProvider<RememberMe, bool>.internal(
+  RememberMe.new,
+  name: r'rememberMeProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$rememberMeHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$RememberMe = AutoDisposeNotifier<bool>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
