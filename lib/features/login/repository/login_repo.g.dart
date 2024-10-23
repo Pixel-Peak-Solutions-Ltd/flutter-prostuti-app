c258bc84c4f8c3856da5c161848f70d449655c68
Abijit Barua <Abijitbarua000@gmail.com>
2024-10-23 22:59:46 +0600

    Refresh token call added.

---
+++ lib/features/login/repository/login_repo.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'login_repo.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$loginRepoHash() => r'aee4a03582d44205b529e9e35af2eef2c0f04e8b';
+
+/// See also [loginRepo].
+@ProviderFor(loginRepo)
+final loginRepoProvider = AutoDisposeProvider<LoginRepo>.internal(
+  loginRepo,
+  name: r'loginRepoProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$loginRepoHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef LoginRepoRef = AutoDisposeProviderRef<LoginRepo>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
