4588d2b10b236f6aa34bbe8ca09e856d39c055a7
saif64s <my@wo.rk>
2024-12-09 22:26:08 +0600

    assignment done

---
+++ lib/features/course/materials/assignment/viewmodel/assignment_viewmodel.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'assignment_viewmodel.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$assignmentViewmodelHash() =>
+    r'92393c1585f2f50164f5264c6dd38fe6acd4a538';
+
+/// See also [AssignmentViewmodel].
+@ProviderFor(AssignmentViewmodel)
+final assignmentViewmodelProvider = AutoDisposeAsyncNotifierProvider<
+    AssignmentViewmodel, List<AssignmentListData>>.internal(
+  AssignmentViewmodel.new,
+  name: r'assignmentViewmodelProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$assignmentViewmodelHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$AssignmentViewmodel
+    = AutoDisposeAsyncNotifier<List<AssignmentListData>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
---
+++ lib/features/course/materials/assignment/viewmodel/get_assignment_by_id.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'get_assignment_by_id.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$getAssignmentByIdHash() => r'f17c906b570f5f6de41fb0ea548c9e6cfce28d60';
+
+/// See also [GetAssignmentById].
+@ProviderFor(GetAssignmentById)
+final getAssignmentByIdProvider =
+    AutoDisposeNotifierProvider<GetAssignmentById, String>.internal(
+  GetAssignmentById.new,
+  name: r'getAssignmentByIdProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$getAssignmentByIdHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$GetAssignmentById = AutoDisposeNotifier<String>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
