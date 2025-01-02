ca69f973843168fb83990a4ccaa5be2366fcffd6
Abijit Barua <Abijitbarua000@gmail.com>
2025-01-02 14:56:01 +0600

    course payment issue fixed.

--- lib/features/payment/view/easy_checkout.dart
+++ lib/features/payment/view/easy_checkout.dart
@@ -4,33 +4,49 @@ import 'package:prostuti/core/services/nav.dart';
 import 'package:prostuti/features/course/my_course/view/my_course_view.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class EasyCheckout extends StatelessWidget {
+class EasyCheckout extends StatefulWidget {
   final String url;
 
-  const EasyCheckout({super.key, required this.url});
+  const EasyCheckout({Key? key, required this.url}) : super(key: key);
+
+  @override
+  State<EasyCheckout> createState() => _EasyCheckoutState();
+}
+
+class _EasyCheckoutState extends State<EasyCheckout> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(Colors.white)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onUrlChange: (change) {
+            print("change.url - ${change.url}");
+            if (change.url!.contains("https://www.google.com/payment/success")) {
+              Nav().pushReplacement(MyCourseView());
+            } else if (change.url!.contains("https://www.google.com/payment/failed")) {
+              Nav().pop();
+              Fluttertoast.showToast(msg: "Failed to purchase the course.");
+            } else if (change.url!.contains("https://www.google.com/payment/cancelled")) {
+              Nav().pop();
+            }
+
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse(widget.url));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       body: SafeArea(
-        child: WebView(
-          initialUrl: url,
-          javascriptMode: JavascriptMode.unrestricted,
-          gestureNavigationEnabled: true,
-          backgroundColor: Colors.white,
-          onPageStarted: (String url) {
-            print(url);
-            if (url.contains("paid/success")) {
-              Nav().pushReplacement(MyCourseView());
-            } else if (url.contains("paid/failed")) {
-              Nav().pop();
-              Fluttertoast.showToast(msg: "Failed to purchase the course.");
-            } else if (url.contains("paid/cancelled")) {
-              Nav().pop();
-            }
-          },
-        ),
+        child: WebViewWidget(controller: _controller),
       ),
     );
   }
