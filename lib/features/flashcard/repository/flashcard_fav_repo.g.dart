3be6604e34f746e46811b180520fced85f9a5bce
saif64s <my@wo.rk>
2025-04-09 18:10:24 +0600

    basic favourite from profile done

---
+++ lib/features/flashcard/repository/flashcard_fav_repo.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'flashcard_fav_repo.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$favoriteFlashcardDetailsHash() =>
+    r'c2d8b72e8c96ef7bdec6cf7c1c50d4ac5582c8e7';
+
+/// See also [FavoriteFlashcardDetails].
+@ProviderFor(FavoriteFlashcardDetails)
+final favoriteFlashcardDetailsProvider = AutoDisposeAsyncNotifierProvider<
+    FavoriteFlashcardDetails, List<FlashcardItem>>.internal(
+  FavoriteFlashcardDetails.new,
+  name: r'favoriteFlashcardDetailsProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$favoriteFlashcardDetailsHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$FavoriteFlashcardDetails
+    = AutoDisposeAsyncNotifier<List<FlashcardItem>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
